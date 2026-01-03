b81340946b296c023ea6a5534e22b97b2ac32804
Edwin jose <edwinjose82429@gmail.com>
2026-01-03 13:20:20 +0530

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Ltd.safco.com">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
